d8d7c3434104479cec22155edcbfaaa0306f7842
Francisco Ferreira <fferre8@cs.mcgill.ca>
2016-01-22 16:21:16 -0500

    Advanced the count example after HOURS of struggling with a forgotten rec.

--- examples/count.ml
+++ examples/count.ml
@@ -13,19 +13,26 @@ lam : (tm -> tm) -> tm.
    not work for applications
  *)
 
-let count [@type "g. {|g, x : tm |- tm|} -> int"] = function
-  (* | {p| g, x : tm |- app 'm 'n |p} -> count m + count n *)
+let rec count [@type "g. {|g, x : tm |- tm|} -> int"] =
+  function
+  | {p| g, x : tm |- c |p} -> 0
+  | {p| g, x : tm |- app 'm 'n |p} -> count m + count n
+  | {p| g, x : tm |- lam (\y. 'm) |p} -> count {t| 'm |t}
   | {p| g, x : tm |- x |p} -> 1
-  | _ -> 0
+  | _ -> 0 (* this cases matches variables that are not the top one *)
 
 let t0 = {t| x : tm |- x |t}
-let t1  = {t| x : tm |- c |t}
+let t1 = {t| x : tm |- c |t}
 let t2 = {t| x : tm |- lam (\y. app x y) |t}
 let t3 = {t| x : tm |- lam (\x. app x x) |t}
-(* let t4 [@type "{|tm|}"]  = {t| x : tm |- lam (\y. app (lam (\z . app(app (app x z) y) x))) |t} *)
+let t4 [@type "{|x : tm |- tm|}"] = {t| x : tm |- lam (\y. app (lam (\z. app(app (app x z) y) x)) x) |t}
+let t5 = {t| x: tm |- app x x |t}
+let t6 = {t| ., x:tm, y:ym |- x |t}
 
-let c0 = count t0
-let c1 = count t1
-let c2 = count t2
-let c3 = count t3
-(* let c4 = count t4 *)
+let c0 = count t0 (* 1 *)
+let c1 = count t1 (* 0 *)
+let c2 = count t2 (* 1 *)
+let c3 = count t3 (* 0 *)
+let c4 = count t4 (* 3 *)
+let c5 = count t5 (* 2 *)
+let c6 = count t6 (* 0 *)
