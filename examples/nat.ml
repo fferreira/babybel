6f9b73571c047e7afd679ebf06f6fd3daf8975f8
Francisco Ferreira <fferre8@cs.mcgill.ca>
2016-01-27 11:54:17 -0500

    Updated the syntax

--- examples/nat.ml
+++ examples/nat.ml
@@ -1,6 +1,11 @@
 open Sf
 
-[@@@signature "nat : type.  z : nat. s : nat -> nat. not : type."]
+[@@@signature {def|
+nat : type.
+z : nat.
+s : nat -> nat.
+
+|def}]
 
 exception Debug
 
@@ -10,7 +15,7 @@ let t2 [@type "{|nat|}"] = {t| z |t}
 let t3 = {t| s (s z) |t}
 let t4 [@type "{|nat|}"] = {t| s (s z) |t}
 
-let rec to_nat [@type "{| . |- nat|} -> int"] = function
+let rec to_nat [@type "{|nat|} -> int"] = function
   | {p| z |p} -> 0
   | {p| s 'n |p} -> 1 + to_nat {t| 'n |t}
   | e -> raise Debug
