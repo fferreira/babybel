0df4cf781fae8996ab6433bd5bbadfb9d028a3a7
Francisco Ferreira <fferre8@cs.mcgill.ca>
2016-03-15 15:13:43 -0400

    in the cps conversion added a phase to apply some of the administrative redices when easy

--- examples/cps2.ml
+++ examples/cps2.ml
@@ -21,12 +21,34 @@ clam   : (cvalue -> kont -> contra) -> cvalue.
 kk: (cvalue -> contra) -> kont.
 adm: kont -> cvalue -> contra.
 
+(* Closure conversion *)
+
+ccontra : type.
+ccvalue : type.
+ckont: type.
+bds : type.
+
+e : (cvalue -> kont -> contra) -> bds.
+c : (cvalue -> bds) -> bds.
+
+sub: type.
+empty : sub.
+dot: sub -> cvalue -> sub.
+
+
+ccapp   : cvalue -> cvalue -> kont -> contra.
+cclam   : {bds} -> cvalue.
+clo: cvalue -> sub -> cvalue.
+
+ckk: (cvalue -> contra) -> kont.
+cadm: kont -> cvalue -> contra.
+
+
 |def}]
 
 type (_, _) rel
   = Empty : (nil, nil) rel
   | Both : ('g, 'd) rel -> (('g, tp_value base) cons, ('d, tp_cvalue base) cons) rel
-
 
 let rec lookup [@type "g d . [g |- value] -> (g, d) rel -> [d |- cvalue]"] =
   fun t -> function
@@ -38,7 +60,6 @@ let rec lookup [@type "g d . [g |- value] -> (g, d) rel -> [d |- cvalue]"] =
 		    let v1 =  lookup {t| #v |t} r'
 		    in {t|*, x |- 'v1 [^1 ;] |t}
 	   end
-
 
 let rec cps [@type "g d . (g, d) rel -> [g |- value] -> [d |- cvalue]"] =
   fun r -> function
@@ -60,6 +81,36 @@ and cpse [@type "g d. (g, d) rel -> [g |- exp] -> [d, k: kont |- contra]"] =
 	   {t| *, k |- 'mm [^1 ; (kk (\f. 'nn [^2 ; (kk (\x. capp f x k))]))] |t}
 	| _ -> assert false
 
+
+(* applies some of the administrative redeces *)
+let rec ar [@type "d. [d, k: kont |- contra] -> [d, k: kont |- contra]"] =
+  function
+  | {p| adm (kk (\vv. 'k1)) 'v |p} -> ar {t|  *, kc|- 'k1 [^1 ; kc ; 'v] |t}
+  | {p| adm #k 'v |p} -> (* it doesn't remove these stuck continuations *)
+     let vv = arv v in
+     {t|adm #k 'vv |t}
+  | {p| capp 'm 'n 'k |p} ->
+     let mm = arv m in
+     let nn = arv n in
+     let kk = ark k in
+     {t| capp 'mm 'nn 'kk |t}
+  | {p| #x |p} -> {t| #x |t}
+
+and arv [@type "d. [d |- cvalue] -> [d |- cvalue]"] =
+  function
+  | {p| clam (\x.\k. 'm) |p} ->
+     let mm = ar m in
+     {t| clam (\x.\k. 'mm) |t}
+  | {p| #x |p} -> {t| #x |t}
+
+and ark [@type "d. [d, k: kont |- kont] -> [d, k: kont |- kont]"] =
+  function
+  | {p| kk (\v. 'mv) |p} ->
+     let mvv = ar {t| *, k, v |-  'mv [^2 ; v ; k] |t} in
+     {t| *, k |- kk (\v. 'mvv[^2 ; v ; k]) |t}
+
 let id_fun = {t| lam (\x. ret x) |t}
 
 let id_fun_k = cps Empty id_fun
+
+let id_fun_k_adm = arv id_fun_k
