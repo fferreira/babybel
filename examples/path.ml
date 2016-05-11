f85bfc75447acae670911607d34ee49799c8cbb0
Francisco Ferreira <fferre8@cs.mcgill.ca>
2016-05-11 17:19:24 -0400

    added new examples

---
+++ examples/path.ml
@@ -0,0 +1,46 @@
+open Sf
+
+[@@@signature {def|
+tm : type.
+c : tm.
+app : tm -> tm -> tm.
+lam : (tm -> tm) -> tm.
+|def}]
+
+exception Not_found
+
+type path
+  = Here
+  | AppL
+  | AppR
+  | InLam
+
+let rec helper [@type "g. [g, x : tm |- tm] -> path list"] =
+function
+| {p| *, x |- c |p} -> raise Not_found
+| {p| *, x |- lam (\y. 'm) |p} -> InLam::(helper {t| *, x, y |- 'm [^2; y ; x] |t})
+| {p| *, x |- x |p} -> [Here]
+| {p| #x |p} -> raise Not_found
+| {p| *, x |- app 'm 'n |p} ->
+   try AppL::(helper m)
+   with _ -> AppR::(helper n)
+
+let get_path [@type "g. [g, x : tm |- tm] -> path list"] = fun t ->
+  try helper t
+  with _ -> []
+
+let t0 = {t| x |- x |t}
+let t1 = {t| x |- c |t}
+let t2 = {t| x |- lam (\y. app x y) |t}
+let t3 = {t| x |- lam (\x. app x x) |t}
+let t4 [@type "[x |- tm]"] = {t| x |- lam (\y. app (lam (\z. app(app (app z z) y) x)) x) |t}
+let t5 = {t| x |- app x x |t}
+let t6 = {t| *, x, y |- x |t}
+
+let c0 = get_path t0
+let c1 = get_path t1
+let c2 = get_path t2
+let c3 = get_path t3
+let c4 = get_path t4
+let c5 = get_path t5
+let c6 = get_path t6
---
+++ examples/rewrite.ml
@@ -0,0 +1,40 @@
+open Sf
+
+[@@@signature {def|
+tm : type.
+i : tm.
+pair : tm -> tm -> tm.
+fst : tm -> tm.
+snd : tm -> tm.
+letpair : tm -> (tm -> tm -> tm) -> tm.
+app : tm -> tm -> tm.
+lam : (tm -> tm) -> tm.
+
+|def}]
+
+let rec rewrite [@type "g. [g |- tm] -> [g |- tm]"] = function
+| {p| i |p} -> {t| i |t}
+| {p| pair 'm 'n |p} ->
+   let mm = rewrite m in
+   let nn = rewrite n in
+   {t| pair 'mm 'nn |t}
+| {p| fst 'm |p} ->
+   let mm = rewrite m in
+   {t| fst 'mm |t}
+| {p| snd 'm |p} ->
+   let mm = rewrite m in
+   {t| snd 'mm |t}
+| {p| letpair 'm (\x.\y. 'n) |p} ->
+   let mm = rewrite m in
+   rewrite {t| 'n [fst 'mm ; snd 'mm] |t}
+| {p| app 'm 'n |p} ->
+   let mm = rewrite m in
+   let nn = rewrite n in
+   {t| app 'mm 'nn |t}
+| {p| lam (\x. 'm) |p} ->
+   let mm = rewrite m in
+   {t| lam (\x. 'mm) |t}
+| {p| #x |p} -> {t| #x |t}
+
+let t0 = rewrite {t| lam (\x. x) |t}
+let t1 = rewrite {t| lam (\x. letpair x (\y.\z. app y z)) |t}
